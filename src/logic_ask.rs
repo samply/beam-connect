feba785b77efda20ffe14039c317162dd20afae2
janskiba <jan.skiba@dkfz-heidelberg.de>
2023-04-26 13:01:56 +0000

    remove check for proxy auth header

--- src/logic_ask.rs
+++ src/logic_ask.rs
@@ -1,5 +1,6 @@
 use std::{sync::Arc, str::FromStr};
 use std::time::{Duration, SystemTime};
+use hyper::http::HeaderValue;
 use hyper::{Request, Body, Client, client::HttpConnector, Response, header, StatusCode, body, Uri};
 use hyper_proxy::ProxyConnector;
 use hyper_tls::HttpsConnector;
@@ -25,9 +26,9 @@ pub(crate) async fn handler_http(
 
     headers.insert(header::VIA, format!("Via: Samply.Beam.Connect/0.1 {}", config.my_app_id).parse().unwrap());
 
-    let auth = 
-        headers.remove(header::PROXY_AUTHORIZATION)
-            .ok_or(StatusCode::PROXY_AUTHENTICATION_REQUIRED)?;
+    let auth = headers
+        .remove(header::PROXY_AUTHORIZATION)
+        .unwrap_or(config.proxy_auth.parse().expect("Proxy auth header could not be generated."));
 
     // Re-pack Authorization: Not necessary since we're not looking at the Authorization header.
     // if headers.remove(header::AUTHORIZATION).is_some() {
