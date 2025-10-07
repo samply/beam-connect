adac77768cebee24047631cedaef8b2eb17f5605
Jan Skiba <jan.skiba@dkfz-heidelberg.de>
2025-10-07 07:29:07 +0000

    chore: bump `build-data`

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 fn main() {
-    build_data::set_GIT_COMMIT_SHORT();
-    build_data::set_GIT_DIRTY();
+    build_data::set_GIT_COMMIT_SHORT().expect("Could not determine git commit");
+    build_data::set_GIT_DIRTY().unwrap();
     build_data::set_BUILD_DATE();
     build_data::set_BUILD_TIME();
     // build_data::no_debug_rebuilds();
